1d8d0a617e5cb703bfa40f092088f03a5dab2430
r-mcgrath <97359037+r-mcgrath@users.noreply.github.com>
2024-05-06 10:28:24 -0400

    fix remove duplicates

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,10 +1,14 @@
 const removeDuplicates = (nums) => {
-  let newSet = [...new Set(nums)]
-  console.log(newSet)
-  nums = newSet
-  let k = newSet.length;
-  return k
-}
+    for(let i=0;i<nums.length;){
+        if(nums[i] ===nums[i+1]){
+            nums.splice(i,1)
+        }else{
+            i++
+        }
+    }
+    return nums.length
+   };
+
 
 let nums1 = [1,1,2];
 let nums2 = [0,0,1,1,1,2,2,3,3,4];
