518868f73ba514e78849322efa5f04ea74ded47c
r-mcgrath <97359037+r-mcgrath@users.noreply.github.com>
2024-05-07 13:02:28 -0400

    singleNumberII

---
+++ singleNumberII.js
@@ -0,0 +1,17 @@
+const singleNumber = (nums) => {
+  let sortedNums = nums.sort();
+  
+  for(let i = 0; i < sortedNums.length; i++) {
+    if (sortedNums[i] === sortedNums[i+1]) {
+        i += 2
+    } else {
+        return sortedNums[i];
+    }
+  }
+};
+
+let nums1 = [2,2,3,2];
+let nums2 = [0,1,0,1,0,1,99];
+
+console.log(singleNumber(nums1));
+console.log(singleNumber(nums2));
