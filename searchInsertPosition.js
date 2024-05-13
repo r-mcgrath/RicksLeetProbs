625a69246c2a9a0eeaf74c3028e3a9d4129f1546
r-mcgrath <97359037+r-mcgrath@users.noreply.github.com>
2024-05-13 15:03:05 -0400

    binary solution

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -1,16 +1,38 @@
-const searchInsert = (nums, target) => {
+// const searchInsert = (nums, target) => {
 
-    for (let i = 0; i<nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
-        } else if (nums[i] > target) {
-            return i;
-        } else if (target > nums[i] && i === nums.length - 1) {
-            return i + 1;
+//     for (let i = 0; i<nums.length; i++) {
+//         if (nums[i] === target) {
+//             return i;
+//         } else if (nums[i] > target) {
+//             return i;
+//         } else if (target > nums[i] && i === nums.length - 1) {
+//             return i + 1;
+//         }
+
+//     }
+
+// }
+
+
+const searchInsert =(nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-
     }
 
+    // If the loop terminates, the target is not found
+    // The correct position for insertion is at 'left'
+    return left;
 }
 
 let nums1 = [1,3,5,6]
