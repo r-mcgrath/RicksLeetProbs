94657edd08d1450007ab8d5ded9e80d5eddd2086
r-mcgrath <97359037+r-mcgrath@users.noreply.github.com>
2024-05-20 15:23:21 -0400

    romans

--- integerToRoman.js
+++ integerToRoman.js
@@ -9,27 +9,55 @@ const intToRoman = (num) => {
         'M': 1000
       };
     
-   numArray = num.toString().split('').reverse()
-       let answer = "";
+   numArray = num.toString().split('')
+       let answer = [];
 
 
   for (let i = 0; i < numArray.length; i++) {
-    let temp = numArray[i] * 10^i;
-    while (temp > 0) {
+    
+    let length = numArray.length + 1
+    let power = length - i;
+    let temp = numArray[i] * 10^power;
+    if (temp === 0) {
+        i++
+    }
+    
+    while(temp > 0)  
+        if (temp >= 1000) {
+            temp -= 1000;
+            answer.push('M');
+        } else if (temp >= 500) {
+            temp -= 500;
+            answer.push('D');
+        } else if (temp >=100) {
+            temp -= 100;
+            answer.push('C');
+        } else if (temp >= 50) {
+            temp -= 50;
+            answer.push('L');
+        } else if (temp >= 10) {
+            temp -= 10;
+            answer.push('X');
+        } else if (temp > 0) {
+            temp -= 1;
+            answer.push('I');
+        }
         
+        console.log(answer)
     }
     
     
-  }
+  
 
 
 
    
-   return numArray;
+   return answer.reverse().join('');
 }
 
 let num1 = 3749;
 let num2 = 58;
 let num3 = 1994;
+let num4 = 1;
 
-console.log(intToRoman(num1))
+console.log(intToRoman(num4))
